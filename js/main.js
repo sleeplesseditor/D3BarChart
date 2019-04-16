283ba7f06c1d57292e47e5f06c43ba6ca85773ad
Josh Bryson <josh@bryson.me.uk>
2019-04-16 12:02:48 +0100

    Flag variable added to move between Profit and Revenue data

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var margin = { left:80, right:20, top:50, bottom:100 };
 var width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
+var flag = true;
+
 var g = d3.select("#chart-area")
     .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -35,7 +37,7 @@ g.append("text")
     .text("Month");
 
 //Y Label
-g.append("text")
+var yLabel = g.append("text")
     .attr("y", - 60)
     .attr("x", - (height / 2))
     .attr("font-size", "20px")
@@ -46,12 +48,15 @@ g.append("text")
 d3.json('data/revenues.json').then(function(data){
     console.log(data);
 
+    // Clean Data
     data.forEach(function(d){
         d.revenue = +d.revenue;
+        d.profit = +d.profit;
     });
 
     d3.interval(function(){
         update(data)
+        flag = !flag;
     }, 1000);
 
     update(data);  
@@ -59,9 +64,10 @@ d3.json('data/revenues.json').then(function(data){
 
 function update(data){
     // console.log('Data updated');
+    var value = flag ? "revenue" : "profit";
 
     x.domain(data.map(function(d){ return d.month }));
-    y.domain([0, d3.max(data, function(d){ return d.revenue })]);
+    y.domain([0, d3.max(data, function(d){ return d[value] })]);
 
     // X-Axis
     var xAxisCall = d3.axisBottom(x)
@@ -76,21 +82,29 @@ function update(data){
         });
         yAxisGroup.call(yAxisCall)
 
-    //  Bar creation
-    // var rects = g.selectAll("rect")
-    //     .data(data)
+    // JOIN
+    var rects = g.selectAll("rect")
+        .data(data);
+    
+    // EXIT
+    rects.exit().remove();
 
-    // rects.enter()
-    //     .append("rect")
-    //         .attr("y", function(d){
-    //             return y(d.revenue);
-    //         })
-    //         .attr("x", function(d){
-    //             return x(d.month);
-    //         })
-    //         .attr("height", function(d){
-    //             return height - y(d.revenue);
-    //         })
-    //         .attr("width", x.bandwidth)
-    //         .attr("fill", "grey");
+    // UPDATE
+    rects
+        .attr("y", function(d){ return y(d[value]) })
+        .attr("x", function(d){ return x(d.month) })
+        .attr("height", function(d){ return height - y(d[value]) })
+        .attr("width", x.bandwidth);
+
+    // ENTER
+    rects.enter()
+        .append("rect")
+            .attr("y", function(d){ return y(d[value]) })
+            .attr("x", function(d){ return x(d.month) })
+            .attr("height", function(d){ return height - y(d[value]) })
+            .attr("width", x.bandwidth)
+            .attr("fill", "grey");
+
+    var label = flag ? "Revenue" : "Profit";
+    yLabel.text(label);
 }
