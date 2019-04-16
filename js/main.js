fd2eaf184a862903e9f4ffde34e3d6df999fca0c
Josh Bryson <josh@bryson.me.uk>
2019-04-16 10:03:35 +0100

    Dynamic update for axis added

--- js/main.js
+++ js/main.js
@@ -10,6 +10,22 @@ var g = d3.select("#chart-area")
     .append("g")
         .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
+var xAxisGroup = g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height +")");
+
+var yAxisGroup = g.append("g")
+    .attr("class", "y axis");
+
+// X Scale
+var x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2);
+
+// Y Scale
+var y = d3.scaleLinear()
+    .range([height, 0]);
+
 // X Label
 g.append("text")
     .attr("y", height + 50)
@@ -34,50 +50,47 @@ d3.json('data/revenues.json').then(function(data){
         d.revenue = +d.revenue;
     });
 
-    // X Scale
-    var x = d3.scaleBand()
-        .domain(data.map(function(d){ return d.month }))
-        .range([0, width])
-        .padding(0.2);
+    d3.interval(function(){
+        update(data)
+    }, 1000);
 
-    // Y Scale
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d){ return d.revenue })])
-        .range([height, 0]);
+    update(data);  
+});
+
+function update(data){
+    // console.log('Data updated');
+
+    x.domain(data.map(function(d){ return d.month }));
+    y.domain([0, d3.max(data, function(d){ return d.revenue })]);
 
     // X-Axis
-    var xAxisCall = d3.axisBottom(x);
-    g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxisCall)
-        .selectAll("text")
-            .attr("y", "17")
+    var xAxisCall = d3.axisBottom(x)
+        xAxisGroup.call(xAxisCall);
+        // .selectAll("text")
+        //     .attr("y", "17")
 
     // Y-Axis
     var yAxisCall = d3.axisLeft(y)
         .tickFormat(function(d){
             return "$" + d;
         });
-    g.append("g")
-        .attr("class", "y axis")
-        .call(yAxisCall)
-    
-    //  Bar creation
-    var rects = g.selectAll("rect")
-        .data(data)
+        yAxisGroup.call(yAxisCall)
 
-    rects.enter()
-        .append("rect")
-            .attr("y", function(d){
-                return y(d.revenue);
-            })
-            .attr("x", function(d){
-                return x(d.month);
-            })
-            .attr("height", function(d){
-                return height - y(d.revenue);
-            })
-            .attr("width", x.bandwidth)
-            .attr("fill", "grey");
-})
+    //  Bar creation
+    // var rects = g.selectAll("rect")
+    //     .data(data)
+
+    // rects.enter()
+    //     .append("rect")
+    //         .attr("y", function(d){
+    //             return y(d.revenue);
+    //         })
+    //         .attr("x", function(d){
+    //             return x(d.month);
+    //         })
+    //         .attr("height", function(d){
+    //             return height - y(d.revenue);
+    //         })
+    //         .attr("width", x.bandwidth)
+    //         .attr("fill", "grey");
+}
